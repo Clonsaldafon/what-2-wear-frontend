fca154a0d8991b41dbba21565ca2f1e0236b484a
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2026-04-10 12:20:21 +0500

    feat (docker): Добавил Dockerfile

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,7 +6,7 @@ import { useAuthStore } from '@/stores/auth'
 import { useErrorStore } from '@/stores/error'
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: (import.meta.env.DEV ? 'http://localhost:8000/api' : '/api'),
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
